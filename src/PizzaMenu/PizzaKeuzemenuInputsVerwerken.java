7047c1769857b65685da705faa2c92418f069e36
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2022-06-20 01:28:13 +0200

    code smells
    code smells

--- src/PizzaMenu/PizzaKeuzemenuInputsVerwerken.java
+++ src/PizzaMenu/PizzaKeuzemenuInputsVerwerken.java
@@ -1,10 +1,10 @@
 package PizzaMenu;
 
 public class PizzaKeuzemenuInputsVerwerken {
-    public String grootte;
-    public String vleesOpPizza;
-    public String kaaskorst;
-    public String studentenpas;
+    private String grootte;
+    private final String vleesOpPizza;
+    private final String kaaskorst;
+    private final String studentenpas;
 
     public PizzaKeuzemenuInputsVerwerken(String grootte, String vleesOpPizza, String kaaskorst, String studentenpas) {
         this.grootte = grootte;
@@ -27,25 +27,22 @@ public class PizzaKeuzemenuInputsVerwerken {
         }
         return grootte;
     }
-
-    //krijgt een input mee voor vlees op pizza
+    
     public boolean pizzaMeat() {
         return vleesOpPizza.equals("1");
-    }
+    }//krijgt een input mee voor vlees op pizza
 
-    //krijgt een input mee voor standaard deeg of deeg met kaaskorst
-    public boolean pizzaCheeseCrust() {
+    public boolean pizzaCheeseCrust() {    //krijgt een input mee voor standaard deeg of deeg met kaaskorst
         return kaaskorst.equals("2");
     }
 
-    //krijgt een input mee of de gebruiker een studentenpas heeft.
-    public boolean studentpas() {
+
+    public boolean studentpas() {//krijgt een input mee of de gebruiker een studentenpas heeft.
         return studentenpas.equals("1");
     }
 
     public void berekenPizzaPrijs(){
         Pizza pizza = new Pizza();
         System.out.printf("Bedankt voor het invullen van de lijst! Uw pizza kost: %.2f €%n", pizza.getPizzaPrijs(pizzaSize(), pizzaMeat(), pizzaCheeseCrust(), studentpas()));
-
     }
 }
